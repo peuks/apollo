e3db306d02e1e26fa0b8e0defc440d1979d9e2d0
peuks <vanmakdavid.lwg@gmail.com>
2021-10-09 16:11:52 +0200

    UPDATE: Better Responsive on Navbar, hero section, services

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 import "./styles/main.scss";
-import bro from "./bro.js";
-document.querySelector("h1").textContent = bro("How's it going");
+import LocomotiveScroll from "locomotive-scroll";
+
+const init = () => {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    touchMultiplier: 2,
+    lerp: 0.1,
+    smartphone: {
+      smooth: true,
+      multiplier: 0.5,
+    },
+    multiplier: 0.85,
+  });
+};
+init();
